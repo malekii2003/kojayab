84c8daf83bffc16a61a53586315f7b1a3d9b64ee
artin <m.malekii2003@gmail.com>
2024-05-12 12:43:41 +0330

    firstcode

---
+++ my-app/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function nutfund() {
+  return (
+  <>
+  <h1>nutfund</h1>
+  
+  
+  
+  
+  </>
+  )
+}
+
+export default nutfund;
---
+++ my-app/pages/index.js
@@ -0,0 +1,18 @@
+
+
+function index() {
+  return (
+  <>
+  <div className="h-[100vh] w-[100%] bg-[#F0F8FF] flex justify-center items-center flex-col  " >
+
+  <img src="imgs/22.jpg" className="h-[400px] w-[800px] rounded-[30px] " ></img>
+<h1 className="mt-[5%] text-[30px] " >این سایت در حال طراحی می باشد و به زودی آپلود میشود</h1>
+  </div>
+  
+
+  
+  </>
+  )
+}
+
+export default index
